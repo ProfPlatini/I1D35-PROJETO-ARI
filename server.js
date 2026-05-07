c7c81ecbfba8e6fb960f42f69f51badd14f806d0
ProfPlatini <platiniluiz@outlook.com>
2026-05-07 10:44:52 -0300

    feat: adiciona dados

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ let historicoSensores = [
     {id:1, temperatura:20, umidade:60, hora:"10:00"},
     {id:2, temperatura:25, umidade:50, hora:"11:00"},
     {id:3, temperatura:30, umidade:40, hora:"12:00"},
-    {id:4, temperatura:40, umidade:50, hora:"13:00"}
+    {id:4, temperatura:40, umidade:50, hora:"13:00"},
+    {id:5, temperatura:45, umidade:10, hora:"14:00"}
 ];
 
 app.get('/api/dados', (req,res) => {
